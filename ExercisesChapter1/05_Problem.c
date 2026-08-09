52985980415f183c98f0df0dbad8d2238d5f2c61
shimulhossain7654-byte <shimulhossain7654@gmail.com>
2026-08-09 23:41:31 +0600

    Update 05_Problem.c

--- ExercisesChapter1/05_Problem.c
+++ ExercisesChapter1/05_Problem.c
@@ -1,23 +1,30 @@
 /*
 5. Take a temperature as input in Fahrenheit, convert it into Celsius and give output.
-   Then, take another temperature in Celsius, convert and output it in Fahrenheit. Use
-   formulas f = 9 × c/5 + 32 and c = (f − 32)/9, where f and c are the Fahrenheit
-   and Celsius temperatures respectively. Use appropriate data types in your program.
+Then, take another temperature in Celsius, convert and output it in Fahrenheit. Use
+formulas f = 9 × c/5 + 32 and c = (f − 32)/9, where f and c are the Fahrenheit
+and Celsius temperatures respectively. Use appropriate data types in your program.
 */
 
 #include <stdio.h>
-int main () 
+
+int main()
 {
-    int f;
+    float f;
     float c;
-    //Farenheit to Celsius
-    printf ("Enter Farenheit temperature: ");
-    scanf ("%i", &f);
-    printf ("Converted Celsius temperature: %f", c = (float)(f - 32)*5 / 9);
-    
-    //Celsius to Farenheit
-    printf ("\nEnter Celsius temperature: ");
-    scanf ("%f", &c);
-    printf ("Converted Farenheit temperature: %i", f = 9 * c / 5 + 32);
+
+    // Fahrenheit to Celsius
+    printf("Enter Fahrenheit temperature: ");
+    scanf("%f", &f);
+
+    c = (f - 32) * 5 / 9;
+    printf("Converted Celsius temperature: %f", c);
+
+    // Celsius to Fahrenheit
+    printf("\nEnter Celsius temperature: ");
+    scanf("%f", &c);
+
+    f = 9 * c / 5 + 32;
+    printf("Converted Fahrenheit temperature: %f", f);
+
     return 0;
 }
