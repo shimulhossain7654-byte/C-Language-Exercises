6d3e8e5bd8da9a435a05bccc9ad74ad4f9125728
shimulhossain7654-byte <shimulhossain7654@gmail.com>
2026-08-09 23:27:41 +0600

    Add comments and instructions for C program
    Added comments and instructions for a C programming exercise.

--- ExercisesChapter1/01_Problem.c
+++ ExercisesChapter1/01_Problem.c
@@ -1,3 +1,11 @@
+/*
+1. Write a program to give output like below. Put proper indentation and give sufficient
+   spacing in your program. Also, write appropriate comments in the program.
+
+   I want to learn C programming.
+   Programming in C is fascinating.
+*/
+
 
 //Example of printf function in C language.
 
