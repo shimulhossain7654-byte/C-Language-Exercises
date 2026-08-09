a781c0be00570680ea3e8cf2125efc75340c544b
shimulhossain7654-byte <shimulhossain7654@gmail.com>
2026-08-09 23:39:33 +0600

    Add comments for velocity and distance calculations
    Add comments to explain the problem and calculations for velocity and distance.

--- ExercisesChapter1/16_Problem.c
+++ ExercisesChapter1/16_Problem.c
@@ -1,3 +1,10 @@
+/*
+16. Let u = 1 be the initial velocity, f = 2 be the acceleration. Compute the velocity
+    v = u + ft and the distance travelled s = ut + 1/2 ft² when t’s values are 0, 1, 2, and
+    3. The output of the program will be two horizontal bar charts, one for the velocity
+    and the other for the distance, where you will use * for each unit in the bar chart.
+*/
+
 #include <stdio.h>
 int main ()
 {
