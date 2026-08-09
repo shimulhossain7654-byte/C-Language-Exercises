240384189e2f73bfa5e80b403e3be67e3d2e6b21
shimulhossain7654-byte <shimulhossain7654@gmail.com>
2026-08-09 23:44:18 +0600

    Update 06_Problem.c

--- ExercisesChapter1/06_Problem.c
+++ ExercisesChapter1/06_Problem.c
@@ -1,31 +1,44 @@
 /*
 6. Take hours, minutes, and seconds as input and convert them into seconds and give
-   output. Also, take the seconds as input, and convert them into hours, minutes, and
-   seconds, and give output. For these, use self referential and compound assignments.
+output. Also, take the seconds as input, and convert them into hours, minutes, and
+seconds, and give output. For these, use self referential and compound assignments.
 */
 
 #include <stdio.h>
-int main ()
+
+int main()
 {
-    int h,m;
+    int h, m;
     float s;
+
     printf("Enter hours: ");
     scanf("%i", &h);
+
     printf("Enter minutes: ");
     scanf("%i", &m);
+
     printf("Enter seconds: ");
     scanf("%f", &s);
-    s = h*3600 + s;
-    s += m*60;
-    printf("Result in second: %f \n",s);
-    printf("Enter second: ");
+
+    s += h * 3600;
+    s += m * 60;
+
+    printf("Result in seconds: %f\n", s);
+
+    printf("Enter seconds: ");
     scanf("%f", &s);
+
     h = s / 3600;
+
     s = (s / 3600) - (int)(s / 3600);
-    s = s * 3600;
-    m = (s / 60);
+    s *= 3600;
+
+    m = s / 60;
+
     s = (s / 60) - (int)(s / 60);
-    s = s * 60;
-    printf("%i hourse %i minutes %f second\n",h,m,s);
+    s *= 60;
+
+    printf("%i hours %i minutes %f seconds\n", h, m, s);
+
     return 0;
 }
