66fc3277b3cc7427d0f71176796ac13f1c87e7f4
shimulhossain7654-byte <shimulhossain7654@gmail.com>
2026-08-09 23:33:32 +0600

    Update 08_Problem.c

--- ExercisesChapter1/08_Problem.c
+++ ExercisesChapter1/08_Problem.c
@@ -1,3 +1,11 @@
+/*
+8. Define three variables a, b, and c, and initialise them with different values. Next,
+   define an additional variable and use it in assignments such that after all the
+   assignments a’s value goes to b, b’s value goes to c, and c’s value goes to a. Write
+   the program in another way, where you will not use any additional variable. Rather
+   you will just use addition and subtraction and still get the same task performed.
+*/
+
 #include <stdio.h>
 int main ()
 {
